7edfba4c0899e1e8ada500a5554c2e2e808b7ac4
Arturo Lopez <huascar.arturo96@gmail.com>
2018-02-19 09:35:02 -0800

    Update App.c

--- Lab-3 CE Optical Sensor Control/App.c
+++ Lab-3 CE Optical Sensor Control/App.c
@@ -1,1 +1,14 @@
 /* Here will go the code for the third lab of the Control Engineering course */
+/* CENTRO DE ENSEÑANZA TÉCNICA Y SUPERIOR
+ * Engineering in Cybernetics Electronics
+ * Subject: Control Engineering
+ * 
+ * Lab Practice 3: Optical Sensor Control
+ * Date: February 9th, 2018
+ * 
+ * Team members: Ana Valeria González
+ *               Arturo López
+ *               César Martínez
+ *               Fernando Raúl Cortez
+ *               Andrés Marín
+ */

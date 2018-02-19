bb008e1afb91d03e9f562b22d8c9ceed2e39b6fb
Arturo Lopez <huascar.arturo96@gmail.com>
2018-02-19 09:37:50 -0800

    Update App.m

--- Lab-2 CE Transfer Functions/App.m
+++ Lab-2 CE Transfer Functions/App.m
@@ -1,15 +1,14 @@
-%{ Here will go the code for the second lab of the Control Engineering course.
- * CENTRO DE ENSEÑANZA TÉCNICA Y SUPERIOR
- * Engineering in Cybernetics Electronics
- * Subject: Control Engineering
- * 
- * Lab Practice 2: Transfer Functions
- * Date: -
- * 
- * Team members: Ana Valeria González
- *               Arturo López
- *               César Martínez
- *               Fernando Raúl Cortez
- *               Andrés Marín
- %}
- 
+% Here will go the code for the second lab of the Control Engineering course.
+% CENTRO DE ENSEÑANZA TÉCNICA Y SUPERIOR
+% Engineering in Cybernetics Electronics
+% Subject: Control Engineering
+% 
+% Lab Practice 2: Transfer Functions
+% Date: -
+% 
+% Team members: Ana Valeria González
+%               Arturo López
+%               César Martínez
+%               Fernando Raúl Cortez
+%               Andrés Marín
+% 

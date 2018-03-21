db8a45159efd1ae127d766ecbf096f614ff71977
Arturo Lopez <huascar.arturo96@gmail.com>
2018-03-20 18:33:32 -0700

    Update main.c

--- Lab-5 EI Introduction to MCU ARM/main.c
+++ Lab-5 EI Introduction to MCU ARM/main.c
@@ -23,6 +23,7 @@
 /* Statics */
 static volatile uint16_t input_ADC;
 static volatile uint16_t ref_ADC;
+static volatile bool mode = true;
 
 int main(void)
 {
@@ -38,6 +39,9 @@ int main(void)
     MAP_GPIO_setAsOutputPin(GPIO_PORT_P6, GPIO_PIN7);
     MAP_GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN3);
     MAP_GPIO_setAsOutputPin(GPIO_PORT_P5, GPIO_PIN1);
+ 
+    /* LED for showing if mode bool is ON/OFF */
+    MAP_GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
  
     /* Configuring P1.1 as an input and enabling interrupts */
     MAP_GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN1);
@@ -93,20 +97,60 @@ int main(void)
     MAP_Interrupt_enableSleepOnIsrExit();
     MAP_Interrupt_enableMaster();
 
-    /* Sleeping when not in use */
+    /* Sleeping when not in use 
     while (1)
     {
         MAP_PCM_gotoLPM0();
-    }
+    }*/
+}
+
+void bar_Display (int,int);
+void dot_Display (int,int);
+
+/* Code for the Bar Display */
+void bar_Display(int input_ADC, int ref_ADC){
+
+}
+
+/* Code for the Dot Display */
+void dot_Display(int input_ADC, int ref_ADC){
+    
 }
 
 void ADC14_IRQHandler(void)
 {
+    /* Check convertion status or the interrupt */
     uint64_t status = MAP_ADC14_getEnabledInterruptStatus();
     MAP_ADC14_clearInterruptFlag(status);
+    /* Get values from the ADC conversion*/
+    input_ADC= MAP_ADC14_getResult(ADC_MEM0);
+    ref_ADC= MAP_ADC14_getResult(ADC_MEM0);
 
     if (ADC_INT1 & status)
     {
+        /* Choose what mode for the display we will be using */
+        if (mode == true){
+            bar_Display(input_ADC,ref_ADC);
+        }
+        else{
+            dot_Display(input_ADC,ref_ADC);
+        }
         MAP_ADC14_toggleConversionTrigger();
     }
+}
+
+void PORT1_IRQHandler(void)
+{
+    /* Check status of the button push interrup */
+    uint32_t status = MAP_GPIO_getEnabledInterruptStatus(GPIO_PORT_P1);
+    MAP_GPIO_clearInterruptFlag(GPIO_PORT_P1, status);
+
+    /* Toggling the output on the LED */
+    if(status & GPIO_PIN1)
+    {
+        /* Toggle the display mode */
+        MAP_GPIO_toggleOutputOnPin(GPIO_PORT_P1, GPIO_PIN0);
+        mode= !mode;
+    }
+
 }
